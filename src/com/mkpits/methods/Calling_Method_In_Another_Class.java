0470e2109d87ce4b2c532174b56246d9345ec548
Vaibhavp911 <vaibhavp9890@gmail.com>
2023-11-28 14:48:47 +0530

    method declaration

---
+++ src/com/mkpits/methods/Calling_Method_In_Another_Class.java
@@ -0,0 +1,20 @@
+package com.mkpits.methods;
+
+public class Calling_Method_In_Another_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
+
+
+
+
+//create class -
+// declare methods
+// getFirstName(), getLastName()
+// getEmail(), getMobile(), getGender(), getPassword()
+// getAllData()
--- src/com/mkpits/variablescope/Static_Variable.java
+++ src/com/mkpits/variablescope/Static_Variable.java
@@ -8,6 +8,7 @@ public class Static_Variable {
 		// variable is declared using static keyword just below class declaration above any method is called static variable.
 		// Static variable are also called class variable because variable scope belong to class itself
 		//:-Value of static variables is sharable by all instance of the class.
+		// calling by direct using, 2 obj.reference
 		Static_Variable variable_Static = new Static_Variable();
 		System.out.println("static_Var1 is "+variable_Static.staticVar1);
 		Non_Static_Variable nsv = new Non_Static_Variable();
