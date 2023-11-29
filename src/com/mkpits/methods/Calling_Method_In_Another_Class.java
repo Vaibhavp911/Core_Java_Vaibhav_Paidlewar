47b83267a4e1f9c87e2f48f038c2249e89af05f7
Vaibhavp911 <vaibhavp9890@gmail.com>
2023-11-29 12:47:26 +0530

    calling method in other class

--- src/com/mkpits/methods/Calling_Method_In_Another_Class.java
+++ src/com/mkpits/methods/Calling_Method_In_Another_Class.java
@@ -5,6 +5,21 @@ public class Calling_Method_In_Another_Class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// Calling method of other class 
+		// calling method using 2 different way
+		
+		// --->  Calling method using class name reference.  
+		System.out.println("------------------------------------------------------------------------");
+		System.out.println("Calling method using class name reference");
+		MethodDeclaration.getAllData();
+		
+		System.out.println("------------------------------------------------------------------------");
+		System.out.println("------------------------------------------------------------------------");
+		// ---> calling method using object reference 
+		System.out.println("calling method using object reference");
+		Calling_Method_In_Another_Class CMAC = new Calling_Method_In_Another_Class();
+		
+		// 
 	}
 	
 	
