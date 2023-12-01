e5a1d857522c24fec41da976ca56f799e2bc18b5
Vaibhavp911 <vaibhavp9890@gmail.com>
2023-12-01 13:19:43 +0530

    test dry file

---
+++ src/com/mkpits/methods/Test_1.java
@@ -0,0 +1,11 @@
+package com.mkpits.methods;
+
+public class Test_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("kfbjvbsbdbvksdjbvsd");
+	}
+
+}
