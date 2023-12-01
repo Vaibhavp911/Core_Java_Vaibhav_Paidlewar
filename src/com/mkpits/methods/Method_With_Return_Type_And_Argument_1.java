5cf4203b8ab4c5274dbfc4656a91d61810c80bb1
Vaibhavp911 <vaibhavp9890@gmail.com>
2023-12-01 14:55:11 +0530

    2nd object created

--- src/com/mkpits/methods/Method_With_Return_Type_And_Argument_1.java
+++ src/com/mkpits/methods/Method_With_Return_Type_And_Argument_1.java
@@ -5,6 +5,8 @@ public class Method_With_Return_Type_And_Argument_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Method_With_Return_Type_And_Argument_1 tB = new Method_With_Return_Type_And_Argument_1(); 
+		Method_With_Return_Type_And_Argument_1 tC = new Method_With_Return_Type_And_Argument_1(); 
+
 	}
 
 }
