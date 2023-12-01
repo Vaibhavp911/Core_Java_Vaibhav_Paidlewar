9f5469d50c1ae7ff7c8558dc0020a36366384463
Vaibhavp911 <vaibhavp9890@gmail.com>
2023-12-01 13:03:03 +0530

    example of method with return type and argument added

---
+++ src/com/mkpits/methods/Method_With_Return_Type_And_Argument.java
@@ -0,0 +1,24 @@
+package com.mkpits.methods;
+
+public class Method_With_Return_Type_And_Argument {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Method_With_Return_Type_And_Argument T = new Method_With_Return_Type_And_Argument();
+		// creating new variable too print 
+		String uDetails = T.getDetails(1, 100000);
+		// printing the method using variable
+		System.out.println(uDetails);
+	}
+
+	private String getDetails(int id, int salary) {
+		// TODO Auto-generated method stub
+		double salary_With_Incentives = salary +  (salary*0.1);
+		// variable "details" has string in it so the datatype will be String
+		String details = "Employee ID : " + id + ", Employee Salary With Incentives : " + salary_With_Incentives;
+		// return is String
+		return details;
+		
+	}
+
+}
