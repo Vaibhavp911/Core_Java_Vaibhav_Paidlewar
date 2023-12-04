cbb5238561840deb779213528c57e6017f853c7c
Vaibhavp911 <vaibhavp9890@gmail.com>
2023-12-04 12:15:33 +0530

    static variable file modified

--- src/com/mkpits/variablescope/Static_Variable.java
+++ src/com/mkpits/variablescope/Static_Variable.java
@@ -8,9 +8,20 @@ public class Static_Variable {
 		// variable is declared using static keyword just below class declaration above any method is called static variable.
 		// Static variable are also called class variable because variable scope belong to class itself
 		//:-Value of static variables is sharable by all instance of the class.
-		// calling by direct using, 2 obj.reference
+		// you can call static variable using 2 methods :-
+		// 1] direct calling in the same class.
+		System.out.println("calling static variable directly in the same class");
+		System.out.println("Static_Var1: "+staticVar1);
+		System.out.println("---------------------------------------------------------------------------------------------------------");
+		// 2] calling using class name.variable name
+		System.out.println("calling static variable using classname.variablename");
+		System.out.println("staticVar1: "+Static_Variable.staticVar1);
+		System.out.println("---------------------------------------------------------------------------------------------------------");
+		// 3] calling static variable using objectname.variablename reference.
 		Static_Variable variable_Static = new Static_Variable();
-		System.out.println("static_Var1 is "+variable_Static.staticVar1);
+		System.out.println("Calling static variable using objectname.variablename reference");
+		System.out.println("static_Var1: "+variable_Static.staticVar1);
+		System.out.println("---------------------------------------------------------------------------------------------------------");
 		Non_Static_Variable nsv = new Non_Static_Variable();
 		System.out.println("calling Non_static variable in static--> Pincode is "+nsv.pinCode);
 		System.out.println("calling Non_static variable in static--> floatNumber is "+nsv.floatNumber);
