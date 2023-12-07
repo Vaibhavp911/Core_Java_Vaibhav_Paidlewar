fae1a78663c7500300073e6ad566400cb26bd1b5
Vaibhavp911 <vaibhavp9890@gmail.com>
2023-12-07 12:59:48 +0530

    calling non-static variable in non-static method

--- src/com/mkpits/variablescope/Non_Static_Variable.java
+++ src/com/mkpits/variablescope/Non_Static_Variable.java
@@ -1,7 +1,8 @@
 package com.mkpits.variablescope;
 
 public class Non_Static_Variable {
-	
+	// note : non- static member cannot be directly called in static member.
+	// note : non-static member can be called directly in non-static member. 
 	public int pinCode = 441111; 	//non static variable as it is not declared static 
 	public float floatNumber = 15.76567f;
 	public byte byteNumber = 122;
@@ -9,6 +10,7 @@ public class Non_Static_Variable {
 	public double doubleNumber = 2535.1423543654657;
 	public long longNumber = 124325436574859086L;
 	public boolean  nonStatic = true;
+	int testVar = 69;//
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Non_Static_Variable variable = new Non_Static_Variable();
@@ -21,7 +23,16 @@ public class Non_Static_Variable {
 		System.out.println("boolean nonStatic "+variable.nonStatic);
 		Static_Variable sv = new Static_Variable();
 		System.out.println("calling static variable in Non-static --> staticVar1: "+sv.staticVar1);
-		
+		System.out.println("-------------------------------------------------------------------------------------");
+		// System.out.print(variable.testCall()); this will show error as i already printed in method.
+		System.out.println("calling non-static in non-static directly");
+		variable.testCall();	// calling non-static in non-static directly.
+	}
+	
+	private void testCall() {
+		// calling non-static member directly in non-static member method 
+		System.out.println(testVar);
+		System.out.println(pinCode);
 	}
 
 }
