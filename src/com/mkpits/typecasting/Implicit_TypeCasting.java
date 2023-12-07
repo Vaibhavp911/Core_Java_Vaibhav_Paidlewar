a260eb27a21a5b638073a4142e1dbea42b8664bd
Vaibhavp911 <vaibhavp9890@gmail.com>
2023-12-07 18:07:40 +0530

    type casting

---
+++ src/com/mkpits/typecasting/Implicit_TypeCasting.java
@@ -0,0 +1,26 @@
+package com.mkpits.typecasting;
+
+public class Implicit_TypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Implicit:-These type of conversion is takes place only if destination type variable is larger than source type variable,
+		// but reverse is not allowed.
+		// e.g
+		 
+		byte a=1;
+		short b= a;	// short size 2 byte
+		int c =  b;		// int size 4 bytes  
+		// you can store smaller size data type in larger size data type ---> these is also known as widening
+		// you not able to store bigger data type in smaller data type
+		// note you can not store short value in short data type
+		long d = c;
+		float f = d; // 2nd highest
+		double e = f;	// 1st highest
+		
+		
+		
+	}
+
+}
