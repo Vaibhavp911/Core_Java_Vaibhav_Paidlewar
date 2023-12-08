7364a9cc388277407c6ed55dc64ac792ef108359
Vaibhavp911 <vaibhavp9890@gmail.com>
2023-12-08 12:47:59 +0530

    explicit type casting

--- src/com/mkpits/typecasting/Explicit_Primitive_To_Object_ViceVersa.java
+++ src/com/mkpits/typecasting/Explicit_Primitive_To_Object_ViceVersa.java
@@ -8,10 +8,30 @@ public class Explicit_Primitive_To_Object_ViceVersa {
 		//Java provide set of pre-defined classes called wrapper classes
 		// String is class
 		// wrapper classes are Integer, Float, Byte, Double, Long, Short, Boolean, Character.
+		System.out.println("----------------------------------------------------------------------------------------");
+		System.out.println("object to primitive data type type-casting");
 		String prise = "236576.353";
 		float p = Float.parseFloat(prise);	// Float wrapper class and parseFloat() is method of Float Wrapper class
 		System.out.println("String 'prise' value assigned to float 'p' is "+p);
-		// console only takes string value to store it in data-type we use TypeCasting between primitive to object & vice versa. 
+		System.out.println("-----------------------------------------------------------------------------------------");
+		// The conversion from String to float involves explicit type casting.
+		// The method Float.parseFloat() is used to explicitly convert the String to a float.
+		// console only takes string value to store it in data-type we use TypeCasting between primitive to object & vice versa.
+		
+		System.out.println("primitive to object type-casting");
+		int a = 35;
+		String getA = Integer.toString(a);
+		System.out.println(getA);
+		System.out.println("------------------------------------------------------------------------------------------");
+		// The conversion from int to String involves explicit type casting.
+		// The method Integer.toString() is used to explicitly convert the int to a String
+		
+		
+		// primitive to object
+		System.out.println("primitive to object type-casting");
+		int i = 43;
+		Integer getI = new Integer(i);
+		System.out.println(getI);
 	}
 
 }
