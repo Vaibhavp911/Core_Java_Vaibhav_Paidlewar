ec4cd0df1deb68e44b679b9ae774157ea2a6ba50
Vaibhavp911 <vaibhavp9890@gmail.com>
2023-12-11 13:19:25 +0530

    overloading of method, "getDetails()"

--- src/com/mkpits/overloading/Method_Overloading.java
+++ src/com/mkpits/overloading/Method_Overloading.java
@@ -2,6 +2,13 @@ package com.mkpits.overloading;
 
 public class Method_Overloading {
 
+	// When more than one method are defined using same name but by passing different types
+	// & different no. of arguments it is called method overloading
+	
+	// It is used to implement static/compile/time polymorphism in java.
+	
+	// Note:-Return type does not play any important role in method overloading
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Employee Details");
@@ -17,12 +24,14 @@ public class Method_Overloading {
 	public void getDetails()
 	{
 		System.out.println("Method with No Argument Passed");
+		System.out.println("------------------------------------------------------------------");
 	}
 	
 	public int getDetails(int id)
 	{
 		System.out.println("Method with one Integer Argument: ");
 		System.out.println("Employee Id: "+id);
+		System.out.println("------------------------------------------------------------------");
 		return id;
 	}
 	
@@ -30,6 +39,7 @@ public class Method_Overloading {
 	{
 		System.out.println("Method with one String Argument: ");
 		System.out.println("Employee Name: "+employeeName);
+		System.out.println("------------------------------------------------------------------");
 		return employeeName;
 	}
 	
@@ -37,6 +47,7 @@ public class Method_Overloading {
 	{
 		System.out.println("Method with one Character Argument: ");
 		System.out.println("Employee Gender: "+employeeGender);
+		System.out.println("------------------------------------------------------------------");
 		return employeeGender;
 	}
 	
@@ -45,8 +56,10 @@ public class Method_Overloading {
 		System.out.println("Method with one boolean Argument: ");
 		if (true) {
 			System.out.println("Permanent Employee");
+			System.out.println("------------------------------------------------------------------");
 		} else {
 			System.out.println("Temprory Employee");
+			System.out.println("------------------------------------------------------------------");
 		}
 		return employeeWorkStatus;
 	}
@@ -56,6 +69,7 @@ public class Method_Overloading {
 		System.out.println("Method with Two Integer Argument: ");
 		System.out.println("Employee Mobile: "+employeeMobile);
 		System.out.println("Employee Salary: "+employeeSalary);
+		System.out.println("------------------------------------------------------------------");
 		String employee_Mob_Sal = " "+employeeMobile+" "+employeeSalary;
 		return employee_Mob_Sal;
 	}
