f40eea4a472d202dc418bb3af988f99f85b6f837
Vaibhavp911 <vaibhavp9890@gmail.com>
2023-12-15 10:18:55 +0530

    sum of n digit

---
+++ src/com/mkpits/controlflowstructure/Sum_Of_Number_ForLoop.java
@@ -0,0 +1,16 @@
+package com.mkpits.controlflowstructure;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Sum_Of_Number_ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter value of n");
+		// int n = 
+
+	}
+
+}
