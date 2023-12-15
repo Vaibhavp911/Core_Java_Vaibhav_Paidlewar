6559202d6aacc52995eba7c5bd4c4054b89dd557
Vaibhavp911 <vaibhavp9890@gmail.com>
2023-12-15 15:44:50 +0530

    equilateral triangle

--- src/com/mkpits/controlflowstructure/For_Loop_Triangle.java
+++ src/com/mkpits/controlflowstructure/For_Loop_Triangle.java
@@ -38,12 +38,18 @@ public class For_Loop_Triangle {
 		
 		System.out.println("-----------------------------------------");
 		
+		System.out.println("Upper Triangle");
 		for (int i = 0 ; i <= a ; i++)
 		{
-			
 			for (int j = 1; j <= (a-i); ++j)
 			{
-				System.out.print("*");
+				System.out.print(" ");
+
+			}
+			
+			for (int j = 0; j < i; j++)
+			{
+				System.out.print(" * ");
 			}
 			System.out.println();
 		}
