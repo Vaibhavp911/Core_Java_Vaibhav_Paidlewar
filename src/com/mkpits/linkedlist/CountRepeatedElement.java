6e3918dfbe468da3b38502e7e45c51ba1418bfc0
Vaibhavp911 <vaibhavp9890@gmail.com>
2024-01-20 23:38:30 +0530

    LinkedList

---
+++ src/com/mkpits/linkedlist/CountRepeatedElement.java
@@ -0,0 +1,40 @@
+package com.mkpits.linkedlist;
+
+import java.util.LinkedList;
+
+public class CountRepeatedElement {
+
+	public static void main(String[] args) 
+	{
+		LinkedList<Integer> listA = new LinkedList<>();
+		listA.add(0, 1);
+		listA.add(1, 2);
+		listA.add(2, 3);
+		listA.add(3, 2);
+		listA.add(4, 2);
+		listA.add(5, 2);
+		listA.add(6, 15);
+		listA.add(7, 7);
+		listA.add(8, 8);
+		
+		byte counter=0;
+		System.out.println(listA.size());
+		
+		for (int i = 0; i < listA.size(); i++) 
+		{
+			
+			for (int j = 8; j > 0; j--) 
+			{
+				if (listA.get(i) == listA.get(j))
+				{
+					System.out.println(listA.get(j));
+					counter++;
+				}
+			}
+			System.out.println("Element "+listA.get(i)+" is repeated "+counter+" times");
+		}
+		
+		
+	}
+
+}
